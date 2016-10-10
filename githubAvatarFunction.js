b505032030d652dfb6bdd38382458fdb8d36b430
Noelle <perdue.noelle@gmail.com>
2016-10-10 20:32:19 +0000

    updated avatar project

--- githubAvatarFunction.js
+++ githubAvatarFunction.js
@@ -1,12 +1,11 @@
-const request = require("request");
+const request = require('request');
+const fs = require('fs');
 
-ar githubRequest = function(endpoint, callback) {
-  console.log(`https://api.github.com${endpoint}`)
+const githubRequest = function(endpoint, callback) {
   var requestData = {
     url: `https://api.github.com${endpoint}`,
     auth: {
-    bearer: process.env.DB_BEARER,
-    },
+    bearer: '05eec1aea8e13f3adaa10753783eeae25fe5ed54'    },
     headers: {
       'User-Agent': "request"
     }
@@ -14,21 +13,32 @@ ar githubRequest = function(endpoint, callback) {
   request.get(requestData, callback)
 }
 
-function getRepoContributors(repoOwner, repoName, callback) {
-  githubRequest(`/repos/${repoOwner}/${repoName}/contributors`, (err, response, body) => {
-    var data = JSON.parse(body);
-  });
-  }
-// function getRepoContributors(repoOwner, repoName, cb) {
-//   githubRequest(`/repos/:owner/:repo/contributors`, (err, response, body) => {
-// }
-// getRepoContributors("lighthouse-labs", "laser_shark", (err, result) => {
-//   console.log("Errors:", err);
-//   console.log("Result:", result);
-// });
-
-function downloadImageByURL(url, filePath) {
+function downloadImageByURL(url, path) {
   console.log(url);
   console.log(path);
-}
+
+  var file = fs.createWriteStream(`./img/${path}.jpg`);
+  request(url).pipe(file);
+
+};
+
+function getRepoContributors(repoOwner, repoName) {
+  githubRequest(`/repos/${repoOwner}/${repoName}/contributors`, (err, response, body) => {
+    if (err) {
+      console.log("error in getRepoContributors", error);
+      return;
+    } else {
+    var contributors = JSON.parse(body);
+
+    contributors.forEach(function(contributor) {
+      downloadImageByURL(contributor.avatar_url, contributor.login)
+      console.log("path: ", contributor.login)
+    });
+  };
+  })
+};
+
+
   getRepoContributors(process.argv[2], process.argv[3], console.log);
+
+
